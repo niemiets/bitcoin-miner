8888d1d9767a87aa0acb5e71d981e0dc3dc5dc2b
Niemiets <pniemiec89@uczen.tl.krakow.pl>
2025-04-20 21:16:25 +0200

    added important TODO

--- src/main.cpp
+++ src/main.cpp
@@ -10,43 +10,43 @@ int main()
 {
 	const char *HOSTNAME = "135.181.137.135";
 	const char *PORT = "18333";
-	
+
 	int err_code = ERROR_SUCCESS;
-	
+
 	WSADATA init_data;
-	
+
 	err_code = WSAStartup(MAKEWORD(2, 2), &init_data);
-	
+
 	if (err_code != ERROR_SUCCESS)
 	{
 		std::cerr << "Couldn't initiate winsock dll: " << err_code << '\n';
 		return 1;
 	}
-	
+
 	addrinfo hints {};
 	hints.ai_addrlen = 0;
 	hints.ai_canonname = nullptr;
 	hints.ai_addr = nullptr;
 	hints.ai_next = nullptr;
-	
+
 	addrinfo *res = new addrinfo;
-	
+
 	err_code = getaddrinfo(HOSTNAME, PORT, &hints, &res);
-	
+
 	if (err_code != ERROR_SUCCESS)
 	{
 		std::cerr << "Couldn't get address info: " << err_code << '\n';
 		return 1;
 	}
-	
+
 	SOCKET sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	
+
 	if (sock == INVALID_SOCKET)
 	{
 		std::cerr << "Couldn't create socket: " << WSAGetLastError() << '\n';
 		return 1;
 	}
-	
+
 	if (connect(sock, res->ai_addr, static_cast<int>(res->ai_addrlen)) == SOCKET_ERROR)
 	{
 		std::cerr << "Couldn't connect to socket: " << WSAGetLastError() << '\n';
@@ -106,7 +106,7 @@ int main()
 	const char d_start_string[4] = {0x0b, 0x11, 0x09, 0x07};
 	const char d_cmd_name[12] = "version\0\0\0\0";
 	const uint32_t d_payload_size = payload_stream.span().size();
-	const char d_checksum[4 + 1] = "test";
+	const char d_checksum[4 + 1] = "test"; // TODO: implement SHA256(SHA256(payload))
 
 	header_stream.write((char*)&d_start_string, 4);
 	header_stream.write((char*)&d_cmd_name, 12);
