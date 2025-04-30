6543a6de7a0e2742c6648e59ee493e875ee8e013
Niemiets <pniemiec89@uczen.tl.krakow.pl>
2025-04-30 10:45:00 +0200

    started implementing receiving version message

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
+#include <cassert>
 #include <cstdint>
+#include <cstring>
 #include <ctime>
-#include <memory>
 #include <iostream>
+#include <memory>
+#include <random>
 #include <span>
 #include <spanstream>
 #include <vector>
 
 #include <winsock2.h>
 #include <ws2tcpip.h>
+
 #include "algorithms/crypto.h"
 
 typedef uint8_t byte;
+
+constexpr uint64_t MAX_UINT64 = 0xFFFFFFFFFFFFFFFF;
 
 constexpr uint32_t MAX_SIZE = 0x02000000;
 constexpr uint8_t  MSG_HEADER_SIZE = 4 + 12 + 4 + 4;
@@ -80,6 +86,10 @@ int main()
 
 	const char *HOSTNAME = "16.16.139.80";
 	const char *PORT	 = "8333";
+
+	std::random_device rand_dev;
+	std::mt19937_64 rng(rand_dev());
+	std::uniform_int_distribution<std::mt19937_64::result_type> nonce_distribution(1, MAX_UINT64);
 
 	int err_code = SUCCESS;
 
@@ -154,7 +164,7 @@ int main()
 	constexpr char    *addr_trans_ip = "";
 	constexpr uint16_t addr_trans_port = 8333;
 
-	constexpr uint64_t nonce = 0x000072616E646F6D;
+	const     uint64_t nonce = nonce_distribution(rng);
 
 	constexpr char   *user_agent = "";
 	const     uint8_t user_agent_bytes = strnlen_s(user_agent, MAX_SIZE);
@@ -200,17 +210,19 @@ int main()
 	.write(reinterpret_cast<const char*>(command_name), 12)
 	.write(reinterpret_cast<const char*>(&payload_size), 4)
 	.write(reinterpret_cast<const char*>(checksum), 4);
+
+	std::cout << "Message header:\n";
 	
 	for (const char &c : header_stream.span())
 	{
-		printf("%02x ", static_cast<uint16_t>(c) & 0xFF);
+		printf("%02x ", static_cast<uint8_t>(c) & 0xFF);
 	}
 	
-	std::cout << '\n';
+	std::cout << "\nMessage payload:\n";;
 
 	for (const char &c : payload_stream.span())
 	{
-		printf("%02x ", static_cast<uint16_t>(c) & 0xFF);
+		printf("%02x ", static_cast<uint8_t>(c) & 0xFF);
 	}
 	
 	std::cout << '\n';
@@ -221,33 +233,134 @@ int main()
 
 	if (bytes_count == SOCKET_ERROR)
 	{
-		std::cerr << "Couldn't send data: " << WSAGetLastError() << '\n';
+		std::cerr << "Couldn't send message header: " << WSAGetLastError() << '\n';
+		freeaddrinfo(res);
+		closesocket(sock);
+		WSACleanup();
 		return 1;
 	}
 	
-	std::cout << "Sent data." << '\n';
+	std::cout << "Sent message header." << '\n';
 	
 	bytes_count = send(sock, payload_stream.span().data(), static_cast<int>(payload_stream.span().size()), 0);
 
 	if (bytes_count == SOCKET_ERROR)
 	{
-		std::cerr << "Couldn't send data: " << WSAGetLastError() << '\n';
+		std::cerr << "Couldn't send message payload: " << WSAGetLastError() << '\n';
+		freeaddrinfo(res);
+		closesocket(sock);
+		WSACleanup();
 		return 1;
 	}
 	
-	std::cout << "Sent data." << '\n';
+	std::cout << "Sent message payload." << '\n';
 	
-	char message_buffer[NETWORK_BUFFER_SIZE];
+	char recv_start_string[4];
 	
-	bytes_count = recv(sock, message_buffer, NETWORK_BUFFER_SIZE, 0);
+	bytes_count = recv(sock, recv_start_string, 4, 0);
 	
 	if (bytes_count == SOCKET_ERROR)
 	{
-		std::cerr << "Couldn't receive data: " << WSAGetLastError() << '\n';
+		std::cerr << "Couldn't receive start string: " << WSAGetLastError() << '\n';
+		freeaddrinfo(res);
+		closesocket(sock);
+		WSACleanup();
 		return 1;
 	}
 	
-	std::cout << "Recieved " << bytes_count << " bytes in message: ";
+	std::cout << "Recieved " << bytes_count << " bytes.\n";
+
+	std::cout << "Recieved start string:\n";
+
+	for (const char &c : recv_start_string)
+	{
+		printf("%02x ", static_cast<uint8_t>(c) & 0xFF);
+	}
+
+	std::cout << '\n';
+
+	char recv_command_name[12];
+
+	bytes_count = recv(sock, recv_command_name, 12, 0);
+
+	if (bytes_count == SOCKET_ERROR)
+	{
+		std::cerr << "Couldn't receive command name: " << WSAGetLastError() << '\n';
+		freeaddrinfo(res);
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Recieved " << bytes_count << " bytes.\n";
+
+	std::cout << "Recieved command name:\n";
+
+	for (const char &c : recv_command_name)
+	{
+		printf("%02x ", static_cast<uint8_t>(c) & 0xFF);
+	}
+
+	std::cout << '\n';
+
+	uint32_t recv_payload_size;
+
+	bytes_count = recv(sock, reinterpret_cast<char*>(&recv_payload_size), 4, 0);
+
+	if (bytes_count == SOCKET_ERROR)
+	{
+		std::cerr << "Couldn't receive payload size: " << WSAGetLastError() << '\n';
+		freeaddrinfo(res);
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Recieved " << bytes_count << " bytes.\n";
+
+	std::cout << "Recieved payload size:\n";
+
+	for (uint8_t i = 0; i < 4; i++)
+	{
+		printf("%02x ", *(reinterpret_cast<uint8_t*>(&recv_payload_size) + i) & 0xFF);
+	}
+
+	std::cout << '\n';
+
+	char recv_checksum[4];
+
+	bytes_count = recv(sock, recv_checksum, 4, 0);
+
+	if (bytes_count == SOCKET_ERROR)
+	{
+		std::cerr << "Couldn't receive checksum: " << WSAGetLastError() << '\n';
+		freeaddrinfo(res);
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Recieved " << bytes_count << " bytes.\n";
+
+	std::cout << "Recieved checksum:\n";
+
+	for (const char &c : recv_checksum)
+	{
+		printf("%02x ", static_cast<uint8_t>(c) & 0xFF);
+	}
+
+	std::cout << '\n';
+
+	std::cout << "Recieved message header.\n";
+
+	if (memcmp(recv_command_name, "version\0\0\0\0\0", 12) != 0)
+	{
+		std::cerr << "Didn't receive correct command name (version): " << WSAGetLastError() << '\n';
+		freeaddrinfo(res);
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
 
 	freeaddrinfo(res);
 	closesocket(sock);
